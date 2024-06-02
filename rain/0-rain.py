210135227721e7cefdfc88f445f46fa7159bfe2a
Amandine0610 <a.irakoze@alustudent.com>
2024-06-02 20:03:38 +0200

    Rain

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,40 +1,33 @@
 #!/usr/bin/python3
-"""calculates how much rain will be trapped after it rains
+"""
+Calculates how much rain will be trapped after it rains.
 """
 
-def rain(walls):
 
+def rain(walls):
     """
     Calculate how many square units of water will be retained after it rains.
-
-    Args:
-        walls (list): A list of non-negative integers representing the heights of walls with unit width 1.
-
-    Returns:
-        int: The total amount of rainwater retained.
     """
-
     if not walls:
         return 0
-    
+
     n = len(walls)
     left_max = [0] * n
     right_max = [0] * n
     water_trapped = 0
-    
+
     # Fill left_max array
     left_max[0] = walls[0]
     for i in range(1, n):
         left_max[i] = max(left_max[i - 1], walls[i])
-    
+
     # Fill right_max array
     right_max[n - 1] = walls[n - 1]
     for i in range(n - 2, -1, -1):
         right_max[i] = max(right_max[i + 1], walls[i])
-    
+
     # Calculate the water trapped
     for i in range(n):
         water_trapped += min(left_max[i], right_max[i]) - walls[i]
-    
-    return water_trapped
 
+    return water_trapped
